7049313f0717140d861903f7841b2a7d3bad2d6d
Nick Huang <nickziyuan.huang@gmail.com>
2024-06-27 19:06:33 -0400

    Updated dataframe structure of results

--- src/legacy/ppi_old_v2.py
+++ src/legacy/ppi_old_v2.py
@@ -24,6 +24,48 @@ MAGENTA = '\033[95m'
 CYAN = '\033[96m'
 RESET = '\033[0m'
 
+def compute_metrics_old(metric_list, estimates_d, config):
+    """
+    A general metric function to be used in the experiment.
+    BIG TODO, WILL NEED A LOT OF WORK
+    Currently only computes mean and CI for width
+
+    Will probably split this function into estimation + CI function
+    and a function to compute the metrics
+
+    params:
+    metric_list (list): list of metrics to compute
+    estimates_d (dict): dictionary of estimates
+    config (dict): configuration for the experiment (probably not needed)
+    """
+    metrics = {}
+    if secondary:
+        metrics['ppi_preds'] = estimates_d['ppi_theta']
+        metrics['ppi_lowers'] = estimates_d['ppi_theta_ci'][0]
+        metrics['ppi_uppers'] = estimates_d['ppi_theta_ci'][1]
+
+        metrics['naive_preds'] = estimates_d['naive_theta']
+        metrics['naive_lowers'] = estimates_d['naive_theta_ci'][0]
+        metrics['naive_uppers'] = estimates_d['naive_theta_ci'][1]
+
+        metrics['classical_preds'] = estimates_d['classical_theta']
+        metrics['classical_lowers'] = estimates_d['classical_ci'][0]
+        metrics['classical_uppers'] = estimates_d['classical_ci'][1]
+    for metric in metric_list:
+        if metric == 'widths':
+            metrics['ppi_widths'] = estimates_d['ppi_theta_ci'][1] - estimates_d['ppi_theta_ci'][0]
+            metrics['naive_widths'] = estimates_d['naive_theta_ci'][1] - estimates_d['naive_theta_ci'][0]
+            metrics['classical_widths'] = estimates_d['classical_ci'][1] - estimates_d['classical_ci'][0]
+        elif metric == 'coverages':
+            true_value = config['experiment']['parameters'].get('true_value', None)
+            if true_value is None:
+                raise ValueError("True value not provided for coverage computation")
+            metrics['ppi_coverages'] = np.mean([1 if true_value >= estimates_d['ppi_theta_ci'][0] and true_value <= estimates_d['ppi_theta_ci'][1] else 0])
+            metrics['naive_coverages'] = np.mean([1 if true_value >= estimates_d['naive_theta_ci'][0] and true_value <= estimates_d['naive_theta_ci'][1] else 0])
+            metrics['classical_coverages'] = np.mean([1 if true_value >= estimates_d['classical_ci'][0] and true_value <= estimates_d['classical_ci'][1] else 0])
+    
+    return metrics
+
 def compute_metrics(config, conf_int):
     """
     Given a confidence interval tuple, computes and returns metrics
@@ -51,7 +93,7 @@ def do_ppi_ci_mean(y_gold, y_gold_fitted, y_fitted, conf, lam=1.0):
     alpha = 1 - conf
     ci = ppi_py.ppi_mean_ci(y_gold, y_gold_fitted, y_fitted, alpha=alpha)
     ci = (ci[0][0], ci[1][0])  # Remove the array
-    return ppi_py.ppi_mean_pointestimate(y_gold, y_gold_fitted, y_fitted)[0], ci
+    return ppi_py.ppi_mean_pointestimate(y_gold, y_gold_fitted, y_fitted)[0], ppi_py.ppi_mean_ci(y_gold, y_gold_fitted, y_fitted, alpha=alpha)
 
 def do_naive_ci_mean(y_gold, y_gold_fitted, y_fitted, conf):
     concat = np.vstack((y_gold, y_fitted)).flatten()  # Concactenate samples
@@ -66,6 +108,42 @@ def do_classical_ci_mean(y_gold, y_gold_fitted, y_fitted, conf):
     h = classical_se * stats.t.ppf((1 + conf) / 2., small_sample-1)  # Highly stolen code, uses t-dist here
     return classical_theta, (classical_theta - h, classical_theta + h)
 
+
+def compute_ci(config, y_gold, y_gold_fitted, y_fitted):
+    """
+    Computes confidence interval and estimate.
+
+    Currently only computes 1 estimate, but hopefully will compute multiple in the future
+
+    return: tuple of (ppi_theta, ppi_theta_ci, naive_theta, naive_theta_ci, classical_theta, classical_ci)
+    """
+    d = {}
+    if config['experiment']['estimate'] == 'mean':
+        conf = config['experiment']['parameters'].get('confidence', 0.9)
+
+        for method in config['experiment']['methods']:
+            if method['type'] == 'ppi':
+                # PPI CI
+                ppi_theta, ppi_theta_ci = do_ppi_ci_mean(y_gold, y_gold_fitted, y_fitted, conf)
+                d['ppi'] = (ppi_theta, ppi_theta_ci)
+            elif method['type'] == 'naive':
+                # Naive CI
+                naive_theta, naive_theta_ci = do_naive_ci_mean(y_gold, y_gold_fitted, y_fitted, conf)
+                d['naive'] = (naive_theta, naive_theta_ci)
+            elif method['type'] == 'classical':
+                # Classical CI
+                classical_theta, classical_ci = do_classical_ci_mean(y_gold, y_gold_fitted, y_fitted, conf)
+                d['classical'] = (classical_theta, classical_ci)
+            elif method['type'] == 'ppi_pp':
+                # PPI++ CI
+                ppi_theta, ppi_theta_ci = do_ppi_ci_mean(y_gold, y_gold_fitted, y_fitted, conf, lam=method['lam'])
+                d['ppi_pp'] = (ppi_theta, ppi_theta_ci)
+            elif method['type'] == 'stratified_ppi':
+                print("Yet to be implemented")
+            else:
+                print("Method not recognized")
+    
+    return d
 
 def compute_ci_singular(config, y_gold, y_gold_fitted, y_fitted, method):
     """
@@ -151,6 +229,84 @@ def extend_metrics(metrics_dict, metrics_d):
             metrics_dict[key].extend(value)
 
     return metrics_dict
+
+def plot_metrics(metrics_dict, config):
+    """
+    Plot the metrics from the experiment, and saves them in the plotting folder
+
+    Add alpha (transparency values)
+
+    Include a few more plots.
+
+    Look at violin plots
+
+    Error bars
+
+    CI Plots
+
+    Biases
+
+    Args:
+    metrics_dict (dict): dictionary of dictionaries of metrics each key is the independent variable
+    config (dict): configuration for the experiment
+    """
+    plt_title = config['experiment']['ind_var']['plot_description']
+    for metric in config['experiment']['metrics']['primary']:
+        if metric == 'widths':
+            plt.figure(dpi=400)
+
+            # Plot each y against x
+            plt.plot(config['experiment']['ind_var']['vals'], metrics_dict['ppi_widths'], label='PPI Widths', marker='o')
+            plt.plot(config['experiment']['ind_var']['vals'], metrics_dict['naive_widths'], label='Naive Widths', marker='x')
+            plt.plot(config['experiment']['ind_var']['vals'], metrics_dict['classical_widths'], label='Classical Widths', marker='s')
+
+            # Add labels and title
+            plt.xlabel(config['experiment']['ind_var']['label_description'])
+            plt.ylabel('Confidence Interval Width')
+            plt.title('Confidence Interval Widths vs Amount of noise')
+            plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+            plt.tight_layout()  # Adjusts the spacing to prevent legend cutoff
+
+            # Save the plot in new experiment folder
+            plt.savefig(os.path.join(config['paths']['plotting_path'], 'widthsplot.png'), bbox_inches='tight')
+
+            # plt.show()
+
+            
+    return 
+
+
+def create_metrics_dict_old(config):
+    """
+    Create a dictionary of metrics to be updated in the experiment
+    """
+    primary_metrics = {}  # metrics to be graphed
+
+    for metric in config['experiment']['metrics'].get('primary', []):
+        primary_metrics["ppi_" + metric] = []
+        primary_metrics["naive_" + metric] = []
+        primary_metrics["classical_" + metric] = [] 
+
+    secondary_metrics = {}  # metrics not to be graphed but to be stored
+
+    # these will always be computed
+
+    secondary_metrics['ppi_preds'] = []
+    secondary_metrics['ppi_lowers'] = []
+    secondary_metrics['ppi_uppers'] = []
+    secondary_metrics['naive_preds'] = []
+    secondary_metrics['naive_lowers'] = []
+    secondary_metrics['naive_uppers'] = []
+    secondary_metrics['classical_preds'] = []
+    secondary_metrics['classical_lowers'] = []
+    secondary_metrics['classical_uppers'] = []
+
+    for metric in config['experiment']['metrics'].get('secondary', []):
+        secondary_metrics["ppi_" + metric] = []
+        secondary_metrics["naive_" + metric] = []
+        secondary_metrics["classical_" + metric] = []
+    
+    return primary_metrics, secondary_metrics
 
 def create_metrics_dict(config):
     """
@@ -240,3 +396,11 @@ def experiment(config):
     print(metrics_df)
 
     return metrics_df
+
+# TODO
+# Change the plotting to be done after everything is put in the dataframe
+# Also, add a plotting section to config
+
+# 1: PPI++ implementation DONE
+# 2: Check if .tocsv is a quick fix DONE
+# 3: Read the experiments email to make sure if code can run the experiments needed.
