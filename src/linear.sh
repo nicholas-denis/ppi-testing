8469b8ac6da0b4880f6303bd4e002614fc090951
Nick Huang <nickziyuan.huang@gmail.com>
2024-07-22 07:17:40 -0700

    huh

--- src/linear.sh
+++ src/linear.sh
@@ -11,16 +11,16 @@ echo "Running experiment 1, linear regression, gold 1024";
 python main.py --config ../configs/Experiment_1/reg_lin_1024.yaml
 
 echo "Running experiment 1, xgboost, gold 8";
-python main.py --config ../configs/Experiment_1/xgboost_8.yaml
+python main.py --config ../configs/Experiment_1/xgb_lin_8.yaml
 
 echo "Running experiment 1, xgboost, gold 16";
-python main.py --config ../configs/Experiment_1/xgboost_16.yaml
+python main.py --config ../configs/Experiment_1/xgb_lin_16.yaml
 
 echo "Running experiment 1, xgboost, gold 128";
-python main.py --config ../configs/Experiment_1/xgboost_128.yaml
+python main.py --config ../configs/Experiment_1/xgb_lin_128.yaml
 
 echo "Running experiment 1, xgboost, gold 1024";
-python main.py --config ../configs/Experiment_1/xgboost_1024.yaml
+python main.py --config ../configs/Experiment_1/xgb_lin_1024.yaml
 
 # chmod +x linear.sh
 # ./linear.sh
