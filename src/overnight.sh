c9d8c9522fd09519fc3821305293ad25c787e8a6
Nick Huang <nickziyuan.huang@gmail.com>
2024-08-14 04:52:51 -0700

    new configs

--- src/overnight.sh
+++ src/overnight.sh
@@ -1,17 +1,23 @@
-echo "Running experiment 3 16g_l_reg";
-python main.py --config ../configs/Experiment_1_new/16g_l_reg.yaml
+echo "Running experiment 1 1kt non lin no clip xgb";
+python main.py --config ../configs/Experiment_2_new/1kt_nc_nonlin_xgb.yaml
 
-echo "Running experiment 4 128g_l_reg";
-python main.py --config ../configs/Experiment_1_new/128g_l_reg.yaml
+echo "Running experiment 2 10kt non lin no clip xgb";
+python main.py --config ../configs/Experiment_2_new/10kt_nc_nonlin_xgb.yaml
 
-echo "Running experiment 5 16g_nl_reg";
-python main.py --config ../configs/Experiment_1_new/16g_nl_reg.yaml
+echo "Running experiment 3 100kt non lin no clip xgb";
+python main.py --config ../configs/Experiment_2_new/100kt_nc_nonlin_xgb.yaml
 
-echo "Running experiment 6 128g_nl_reg";
-python main.py --config ../configs/Experiment_1_new/128g_nl_reg.yaml
+echo "Running experiment 1 1kt non lin clip xgb";
+python main.py --config ../configs/Experiment_2_new/1kt_c_nonlin_xgb.yaml
+
+echo "Running experiment 1 10kt non lin clip xgb";
+python main.py --config ../configs/Experiment_2_new/10kt_c_nonlin_xgb.yaml
+
+echo "Running experiment 1 1kt non lin clip xgb";
+python main.py --config ../configs/Experiment_2_new/100kt_c_nonlin_xgb.yaml
 
 # shut off windows machine after running the script
-#shutdown -s
+shutdown -s
 
 # chmod +x overnight.sh
 # ./overnight.sh
