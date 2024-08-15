661041325af812425ce20d8218bc6d578a3c96bb
Nick Huang <nickziyuan.huang@gmail.com>
2024-08-15 00:04:25 -0700

    Ran experiments, moved files

--- src/overnight.sh
+++ src/overnight.sh
@@ -1,23 +1,26 @@
-echo "Running experiment 1 1kt non lin no clip xgb";
-python main.py --config ../configs/Experiment_2_new/1kt_nc_nonlin_xgb.yaml
+echo "Running experiment 1";
+python main.py --config ../configs/Experiment_1_new/16g_nl_reg.yaml  # random one that needs to be rerun
 
-echo "Running experiment 2 10kt non lin no clip xgb";
-python main.py --config ../configs/Experiment_2_new/10kt_nc_nonlin_xgb.yaml
+echo "Running experiment 2";
+python main.py --config ../configs/Experiment_1_new/512g_l_reg.yaml
 
-echo "Running experiment 3 100kt non lin no clip xgb";
-python main.py --config ../configs/Experiment_2_new/100kt_nc_nonlin_xgb.yaml
+echo "Running experiment 3";
+python main.py --config ../configs/Experiment_1_new/512g_nl_reg.yaml
 
-echo "Running experiment 1 1kt non lin clip xgb";
-python main.py --config ../configs/Experiment_2_new/1kt_c_nonlin_xgb.yaml
+echo "Running experiment 4";
+python main.py --config ../configs/Experiment_1_new/512g_nl_xgb.yaml
 
-echo "Running experiment 1 10kt non lin clip xgb";
-python main.py --config ../configs/Experiment_2_new/10kt_c_nonlin_xgb.yaml
+echo "Running experiment 5";
+python main.py --config ../configs/Experiment_1_new/1024g_l_reg.yaml
 
-echo "Running experiment 1 1kt non lin clip xgb";
-python main.py --config ../configs/Experiment_2_new/100kt_c_nonlin_xgb.yaml
+echo "Running experiment 6";
+python main.py --config ../configs/Experiment_1_new/1024g_nl_reg.yaml
+
+echo "Running experiment 7";
+python main.py --config ../configs/Experiment_1_new/1024g_nl_xgb.yaml
 
 # shut off windows machine after running the script
-shutdown -s
+# shutdown -s
 
 # chmod +x overnight.sh
 # ./overnight.sh
